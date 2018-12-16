7723d189fefa8558b4085ffc9abd2389e41e332a
zip1982b <zip1982b@gmail.com>
2018-12-16 16:01:18 +0200

    FAN init

---
+++ main/main.c
@@ -0,0 +1,47 @@
+/* ventilation system progect
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "driver/gpio.h"
+
+/**
+ * Brief:
+ * This test code configure gpio and use gpio interrupt.
+ *
+ * GPIO status:
+ * GPIO18: output
+ * GPIO19: output
+ * GPIO4:  input, pulled up, interrupt from rising edge and falling edge
+ * GPIO5:  input, pulled up, interrupt from rising edge.
+ *
+ * Test:
+ * Connect GPIO18 with GPIO4
+ * Connect GPIO19 with GPIO5
+ * Generate pulses on GPIO18/19, that triggers interrupt on GPIO4/5
+ *
+ */
+ 
+
+#define ZERO_SENSOR			34
+#define FAN_IN				32
+#define FAN_OUT				33
+#define GPIO_INPUT_PIN_SEL  ((1ULL<<ZERO_SENSOR) | (1ULL<<FAN_IN) | (1ULL<<FAN_OUT))
+#define ESP_INTR_FLAG_DEFAULT 0
+ 
+ 
+
+
+void app_main()
+{
+    
+}
