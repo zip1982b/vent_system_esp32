64e7795b1e34111c533a92b2a901d056e4530e54
zip1982b <zip1982b@gmail.com>
2021-05-14 22:49:12 +0200

    add task mqtt_pub

--- main/main.c
+++ main/main.c
@@ -7,8 +7,22 @@
    CONDITIONS OF ANY KIND, either express or implied.
 */
 #include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
 #include <string.h>
 #include <stdlib.h>
+
+#include "esp_wifi.h"
+#include "esp_system.h"
+
+#include "nvs_flash.h"
+
+#include "esp_event.h"
+
+
+#include "esp_netif.h"
+
+#include "esp_protocol_examples_common.h"
 
 #include <unistd.h>
 #include "esp_timer.h"
@@ -20,8 +34,11 @@
 #include "freertos/semphr.h"
 #include "driver/gpio.h"
 
+#include "lwip/sockets.h"
+#include "lwip/dns.h"
+#include "lwip/netdb.h"
 #include "esp_log.h"
-
+#include "mqtt_client.h"
 #include "DHT.h"
 
 
@@ -46,6 +63,10 @@ static const char* TAG = "VentSys";
 static const char* TAG_dht = "DHT22";
 
 static const char* TAG_reg = "Regulator";
+
+static const char* TAG_mqtt = "MQTT";
+
+
 
 
 #define FAN				32
@@ -94,6 +115,72 @@ xSemaphoreHandle xBinSemaphoreZS;
 		}
 	}
 }
+
+
+static void log_error_if_nonzero(const char *message, int error_code)
+{
+	    if (error_code != 0) {
+		    ESP_LOGE(TAG, "Last error %s: 0x%x", message, error_code);
+	    }
+}
+
+
+
+
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data){
+    ESP_LOGD(TAG_mqtt, "event dispatched from event loop base=%s, event_id=%d", base, event_id);
+    esp_mqtt_event_handle_t event = event_data;
+    esp_mqtt_client_handle_t client = event->client;
+    switch((esp_mqtt_event_id_t)event_id){
+	    case MQTT_EVENT_CONNECTED:
+		    ESP_LOGI(TAG_mqtt, "MQTT_EVENT_CONNECTED");
+		    break;
+	    case MQTT_EVENT_DISCONNECTED:
+		    ESP_LOGI(TAG_mqtt, "MQTT_EVENT_DISCONNECTED");
+		    break;
+	    case MQTT_EVENT_SUBSCRIBED:
+		    ESP_LOGI(TAG_mqtt, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+		    break;
+	    case MQTT_EVENT_UNSUBSCRIBED:
+		    ESP_LOGI(TAG_mqtt, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
+		    break;
+	    case MQTT_EVENT_PUBLISHED:
+		    ESP_LOGI(TAG_mqtt, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+		    break;
+	    case MQTT_EVENT_DATA:
+		    ESP_LOGI(TAG_mqtt, "MQTT_EVENT_DATA");
+		    printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
+		    printf("DATA=%.*s\r\n", event->data_len, event->data);
+                    break;
+	    case MQTT_EVENT_ERROR:
+		    ESP_LOGI(TAG_mqtt, "MQTT_EVENT_ERROR");
+		    if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT) {
+			    log_error_if_nonzero("reported from esp-tls", event->error_handle->esp_tls_last_esp_err);
+			    log_error_if_nonzero("reported from tls stack", event->error_handle->esp_tls_stack_err);
+			    log_error_if_nonzero("captured as transport's socket errno",  event->error_handle->esp_transport_sock_errno);
+			    ESP_LOGI(TAG, "Last errno string (%s)", strerror(event->error_handle->esp_transport_sock_errno));
+		    }
+		    break;
+	    default:
+
+		    ESP_LOGI(TAG_mqtt, "other event id:%d", event->event_id);
+		    break;
+
+    }
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
 
 
 
@@ -202,12 +289,14 @@ void Regulator_task(void *pvParameter)
 {
 
     float target_humidity = 45.2;// %
-    ESP_LOGI(TAG_dht, "Starting DHT Task\n\n");
+    ESP_LOGI(TAG_dht, "Starting DHT Task\n\n"); 
     float H = 0.0;
     uint8_t delta = 2;//default delta
     uint8_t speed = 2;//default speed
+
+
     
-	while(1){
+while(1){
 
     //receive speed from MQTT
 		xQueueReceive(xQueueHumi, &H, 0);
@@ -230,9 +319,46 @@ void Regulator_task(void *pvParameter)
 }
 
 
+void MQTT_pub(void *pvParameter)
+{
+	esp_mqtt_client_config_t mqtt_cfg = {
+		.uri = CONFIG_BROKER_URL,
+	};
+	esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
+	esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
+	esp_mqtt_client_start(client);
+	
+	while(1){
+
+	}	
+
+
+
+
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 void app_main()
 {
+    ESP_LOGI(TAG, "[app] startup ...");
+    ESP_LOGI(TAG, "[app] Free memory: %d bytes", esp_get_free_heap_size());
+    ESP_LOGI(TAG, "[app] IDF version: %s", esp_get_idf_version());
 	/*** triac control ***/
     gpio_config_t io_conf;
     io_conf.intr_type = GPIO_PIN_INTR_DISABLE; //disable interrupt
@@ -290,6 +416,11 @@ void app_main()
 
     xTaskCreate(&DHT_task, "DHT_task", 2048, NULL, 5, NULL);
     xTaskCreate(&Regulator_task, "Regulator Humi", 2048, NULL, 5, NULL);
+
+    xTaskCreate(&MQTT_pub, "MQTT publish task", 2048, NULL, 5, NULL);
+
+    ESP_ERROR_CHECK(example_connect());
+
 
 }
 
